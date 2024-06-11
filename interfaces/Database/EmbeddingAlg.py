9939a7064c7c064ebbb8408f28d57b4bd37f2bf3
Tristan Roulet <troulet27@gmail.com>
2024-06-11 15:39:22 +0200

    fix: remove litteral query string

--- interfaces/Database/EmbeddingAlg.py
+++ interfaces/Database/EmbeddingAlg.py
@@ -1,31 +1,43 @@
 import pickle
-import sqlite3
 import os
 
 from interfaces.Database.EmbeddingDbI import EmbeddingDbI
+from sqlalchemy import Column, Integer, String, BLOB, create_engine, select, insert, update
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Embeddings2(Base):
+    __tablename__ = 'embeddings'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    file_path = Column(String)
+    embedding = Column(BLOB)
 
 class EmbeddingAlg(EmbeddingDbI):
     def __init__(self) -> None:
-        self.conn = sqlite3.connect('Embeddings.db')
-        self.c = self.conn.cursor()
-        self.c.execute('''CREATE TABLE IF NOT EXISTS embeddings (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            file_path TEXT,
-            embedding BLOB
-            )''')
-        self.conn.commit()
+        engine = create_engine('sqlite:///Embeddings2.db')
+        Base.metadata.create_all(engine)
+        self.conn = engine.connect()
     
     def get_embedding(self, file_path, function_name = None):
-        self.c.execute('SELECT embedding FROM embeddings WHERE file_path = ?', (file_path,))
-        row = self.c.fetchone()
+        stmt = select(Embeddings2.embedding).where(Embeddings2.file_path == file_path)
+        results = self.conn.execute(stmt)
+        self.conn.commit()
+        row = results.fetchone()
         if row:
             return pickle.loads(row[0])
         return None
     
     def save_embedding(self, file_path, embedding, function_name = None):
-        self.c.execute('INSERT OR REPLACE INTO Embeddings (file_path, embedding) VALUES (?, ?)', (file_path, pickle.dumps(embedding)))
-        self.conn.commit()
+        new = insert(Embeddings2).values(file_path=file_path, embedding=pickle.dumps(embedding))
+        old = update(Embeddings2).where(Embeddings2.file_path == file_path).values(embedding=pickle.dumps(embedding))
+        sel = self.conn.execute(select(Embeddings2).where(Embeddings2.file_path == file_path))
+        if sel.first() is None:
+            self.conn.execute(new)
+        else:
+            self.conn.execute(old)
     
     def clean(self):
         self.conn.close()
-        os.remove("./Embeddings.db")
+        os.remove("./Embeddings2.db")
--- interfaces/Database/EmbeddingT5.py
+++ interfaces/Database/EmbeddingT5.py
@@ -1,32 +1,44 @@
 import pickle
-import sqlite3
 import os
 
 from interfaces.Database.EmbeddingDbI import EmbeddingDbI
+from sqlalchemy import Column, Integer, String, BLOB, create_engine, select, insert, update
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Embeddings1(Base):
+    __tablename__ = 'embeddings'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    file_path = Column(String)
+    function_name = Column(String)
+    embedding = Column(BLOB)
 
 class EmbeddingT5(EmbeddingDbI):
     def __init__(self) -> None:
-        self.conn = sqlite3.connect('Embeddings.db')
-        self.c = self.conn.cursor()
-        self.c.execute('''CREATE TABLE IF NOT EXISTS embeddings (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            file_path TEXT,
-            function_name TEXT,
-            embedding BLOB
-            )''')
-        self.conn.commit()
+        engine = create_engine('sqlite:///Embeddings1.db')
+        Base.metadata.create_all(engine)
+        self.conn = engine.connect()
     
     def get_embedding(self, file_path, function_name):
-        self.c.execute('SELECT embedding FROM Embeddings WHERE file_path = ? AND function_name = ?', (file_path, function_name))
-        row = self.c.fetchone()
+        stmt = select(Embeddings1.embedding).where(Embeddings1.file_path == file_path and Embeddings1.function_name == function_name)
+        results = self.conn.execute(stmt)
+        self.conn.commit()
+        row = results.fetchone()
         if row:
             return pickle.loads(row[0])
         return None
     
     def save_embedding(self, file_path, function_name, embedding):
-        self.c.execute('INSERT OR REPLACE INTO Embeddings (file_path, function_name, embedding) VALUES (?, ?, ?)', (file_path, function_name, pickle.dumps(embedding)))
-        self.conn.commit()
+        new = insert(Embeddings1).values(file_path=file_path, function_name=function_name, embedding=pickle.dumps(embedding))
+        old = update(Embeddings1).where(Embeddings1.file_path == file_path and Embeddings1.function_name == function_name).values(embedding=pickle.dumps(embedding))
+        sel = self.conn.execute(select(Embeddings1).where(Embeddings1.file_path == file_path and Embeddings1.function_name == function_name))
+        if sel.first() is None:
+            self.conn.execute(new)
+        else:
+            self.conn.execute(old)
     
     def clean(self):
         self.conn.close()
-        os.remove("./Embeddings.db")
+        os.remove("./Embeddings1.db")
